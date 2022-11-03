ee1dd0d4c1925b7431adc5d97da188fa6b2b705a
brenokamura <brenokamura@gmail.com>
2022-11-03 13:52:47 -0300

    Testando funcionalidade dos Post e do Get...ok

--- src/app.js
+++ src/app.js
@@ -2,5 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.post('/sign-up', (req, res) => {
+  res.send("Post sign-up funcionando...");
+});
+
+app.post('/tweets', (req, res) => {
+  res.send("Post tweets funcionando...");
+});
+
+app.get('/tweets', (req, res) => {
+  res.send("Get tweets funcionando...");
+});
 
 app.listen(5000)
