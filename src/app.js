573c1aa231da787029e24522a89237dc3629fed9
brenokamura <brenokamura@gmail.com>
2022-11-04 14:01:22 -0300

    Implementada POST /sign-up pelo body da request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
-  res.send("Post sign-up funcionando...");
+  const { username, avatar } = req.body;
+
+  if (!username) {
+    res.send("Preencha o campo Nome do usuário!");
+    return;
+  }else{
+  }if (!avatar) {
+    res.send("Insira o link do seu avatar!");
+    return;
+  }
+
+  users.push(req.body);
+  res.send("Cadastro realizado com sucesso!");
 });
+
+
 
 app.post('/tweets', (req, res) => {
   res.send("Post tweets funcionando...");
